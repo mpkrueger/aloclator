7ee4520107cdbf045771c8840394324f855c3328
Matt Krueger <mpkrueger@gmail.com>
2015-06-19 11:30:55 -0700

    Added navigation controller and adjusted auto-layout

---
+++ Aloclator/Aloclator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Aloclator
+//
+//  Created by Matt Krueger on 6/17/15.
+//  Copyright (c) 2015 Matt Krueger. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
